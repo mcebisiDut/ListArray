9ad5f2bced308ad184f82f17a57abda808afdecb
mcebisi <mcebisimkhohliwe@gmail.com>
2019-03-15 19:13:32 +0200

    Included 'replace(item)', 'getItem(position )', 'contains(item)' methods

--- Main/src/main/ListArray.java
+++ Main/src/main/ListArray.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
-
 /**
  *
  * @author mcebisim
  */
 public interface ListArray {
+    
     public boolean add(int item);
     public boolean add(int position, int item);
     public int remove(int position);
-    public void clear();
+    public boolean replace(int position, int item);
+    public boolean contains(int item);
+    public int getItem(int position);
     public int getLength();
     public void display();
+    public void clear();
     public boolean isFull();
     public boolean isEmpty();
 }
