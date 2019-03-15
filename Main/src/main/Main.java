0299046d1d0f04f31fabc56253a6a759d425ffc9
mcebisi <mcebisimkhohliwe@gmail.com>
2019-03-15 19:12:14 +0200

    Included 'replace(item)', 'getItem(position)', 'contains(item)'

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -1,26 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
-
 /**
  *
  * @author mcebisim
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         ListArray MyList = new List(20);
         MyList.add(20);
         MyList.add(20);
         MyList.add(1,30);
         MyList.display();
+        System.out.println("===========================");
+        MyList.replace(0, 10);
+        MyList.display();
+        System.out.println(MyList.contains(40));
+        System.out.println(MyList.getItem(0));
         System.out.println("===========================");
         MyList.remove(1);
         MyList.display();
